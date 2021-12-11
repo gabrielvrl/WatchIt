2ae99ce31ce9abff491776d39dde9de7e2562623
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-11 16:21:00 -0300

    Watching Files with Chokidar

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
-console.log('I was executed');
+const chokidar = require('chokidar');
+
+chokidar.watch('.')
+  .on('add', () => console.log('File added'))
+  .on('change', () => console.log('File changed'))
+  .on('unlink', () => console.log('File unlinked'))
