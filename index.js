72127190064f60f80d3e809d5fe79177ca7d43c9
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-11 16:30:23 -0300

    Deboucing and Events

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce')
 const chokidar = require('chokidar');
 
+const start = debounce(() => {
+  console.log('Starting users program');
+}, 100);
+
 chokidar.watch('.')
-  .on('add', () => console.log('File added'))
+  .on('add', start)
   .on('change', () => console.log('File changed'))
   .on('unlink', () => console.log('File unlinked'))
