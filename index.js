2b8ac0d1ad3d9a0c7c06eb36a725cc9e9c3446ae
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-11 16:48:55 -0300

    Ensuring File Exists

--- index.js
+++ index.js
@@ -3,21 +3,29 @@
 const debounce = require('lodash.debounce')
 const chokidar = require('chokidar');
 const program = require('caporal');
-
-const start = debounce(() => {
-  console.log('Starting users program');
-}, 100);
+const fs = require('fs');
 
 program
   .version('0.0.1')
   .argument('[filename]', 'Name of a file to execute')
-  .action((args) => {
-    console.log(args);
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
+
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`)
+    }
+
+    const start = debounce(() => {
+      console.log('Starting users program');
+    }, 100);
+
+    chokidar.watch('.')
+    .on('add', start)
+    .on('change', start)
+    .on('unlink', start);
   });
 
 program.parse(process.argv)
-/* 
-chokidar.watch('.')
-  .on('add', start)
-  .on('change', () => console.log('File changed'))
-  .on('unlink', () => console.log('File unlinked')) */
+
